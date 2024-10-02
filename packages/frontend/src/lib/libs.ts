811c94fb0bea3eb9be792ceeb55f261ca7346e5f
Ayodeji <olusanyasamson633@gmail.com>
2024-10-02 06:04:08 +0100

    changes

--- packages/frontend/src/lib/libs.ts
+++ packages/frontend/src/lib/libs.ts
@@ -1,10 +1,8 @@
 // useFetchGroups.ts
 import { client } from "@/app/client";
 import { contractAddress } from "@/contract";
-import { ContractOptions, getContract } from "thirdweb";
+import { getContract } from "thirdweb";
 import { defineChain } from "thirdweb/chains";
-import { useReadContract } from "thirdweb/react";
-import { useState, useEffect } from "react";
 import { tokenAddress } from "@/token";
 
 export const liskSepolia = defineChain(534351);
